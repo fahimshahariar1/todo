eb4554d7e1b0e7205696ab19a211b27381131217
Fahim <fahimshahariar.fs@gmail.com>
2023-05-12 12:39:01 +0600

    third

--- todo_flutter/lib/widgets/todo_item.dart
+++ todo_flutter/lib/widgets/todo_item.dart
@@ -14,6 +14,7 @@ class TodoItem extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
           Icons.check_box,
@@ -25,6 +26,23 @@ class TodoItem extends StatelessWidget {
               fontSize: 18,
               color: tdBlack,
               decoration: TextDecoration.lineThrough),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                size: 19,
+                Icons.delete,
+                color: Colors.white,
+              )),
         ),
       ),
     );
